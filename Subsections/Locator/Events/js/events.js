910a984df94fa4fa5d9b4411bfbfd895b25eac8c
lisagieng <lisagieng@gmail.com>
2020-03-25 20:28:42 -0400

    Testing more event loading.
    Switched to json file.

--- Subsections/Locator/Events/js/events.js
+++ Subsections/Locator/Events/js/events.js
@@ -1,25 +1,5 @@
-// parsing events.xml file
-google.load("feeds", "1");
-
-   function initialize() {
-     var feed = new google.feeds.Feed("https://raw.githubusercontent.com/lisagieng/ray/master/Subsections/Locator/Events/js/events.xml");
-     feed.load(function(result) {
-       if (!result.error) {
-         var container = document.getElementById("eventsHere");
-         for (var i = 0; i < result.feed.entries.length; i++) {
-           var entry = result.feed.entries[i];
-           var tr = document.createElement("tr");
-           var info = "<td>" + entry.name + "</td><td>" + entry.description + "</td><td>" + entry.date + "</td><td>" + entry.location + "</td><td>" + entry.organizer + "</td><td>" + entry.link + "</td><td>"
-           tr.innerHTML = info;
-           container.appendChild(tr);
-         }
-       }
-     });
-   }
-   google.setOnLoadCallback(initialize);
-
-   // Implementing dropdown menu with hover
-   //https://stackoverflow.com/questions/42183672/how-to-implement-a-navbar-dropdown-hover-in-bootstrap-v4
+// Implementing dropdown menu with hover
+//https://stackoverflow.com/questions/42183672/how-to-implement-a-navbar-dropdown-hover-in-bootstrap-v4
    function toggleDropdown (e) {
      const _d = $(e.target).closest('.dropdown'),
        _m = $('.dropdown-menu', _d);
@@ -34,3 +14,26 @@ google.load("feeds", "1");
    $('body')
      .on('mouseenter mouseleave','.dropdown',toggleDropdown)
      .on('click', '.dropdown-menu a', toggleDropdown);
+
+// parsing json for events
+var xhttp = new XMLHttpRequest();
+xhttp.onload = function() {
+  if(xhttp.status === 200) {
+    responseObject = JSON.parse(xhttp.responseText);
+  }
+
+var events = "";
+
+for(var i=0; i < responseObject.length(); i++){
+  events += "<td>" + responseObject.event[i].name + "</td>";
+  events += "<td>" + responseObject.event[i].description + "</td>";
+  events += "<td>" + responseObject.event[i].date + "</td>";
+  events += "<td>" + responseObject.event[i].location + "</td>";
+  events += "<td>" + responseObject.event[i]. organizer + "</td>";
+  events += "<td>" + responseObject.event[i].link + "</td>";
+}
+
+$("#eventsHere").html(events);
+
+xhttp.open("GET", "js/events.json", true);
+xhttp.send('');
