9d8f2875532ee86c87029b56ff3d2ef39ca01034
lisagieng <lisagieng@gmail.com>
2020-03-25 20:12:38 -0400

    Updated events.xml

--- Subsections/Locator/Events/js/events.js
+++ Subsections/Locator/Events/js/events.js
@@ -2,7 +2,7 @@
 google.load("feeds", "1");
 
    function initialize() {
-     var feed = new google.feeds.Feed("https://www.lisagieng.github.io/ray/Subsections/Locater/Events/js/events.xml");
+     var feed = new google.feeds.Feed("https://raw.githubusercontent.com/lisagieng/ray/master/Subsections/Locator/Events/js/events.xml");
      feed.load(function(result) {
        if (!result.error) {
          var container = document.getElementById("eventsHere");
@@ -17,3 +17,20 @@ google.load("feeds", "1");
      });
    }
    google.setOnLoadCallback(initialize);
+
+   // Implementing dropdown menu with hover
+   //https://stackoverflow.com/questions/42183672/how-to-implement-a-navbar-dropdown-hover-in-bootstrap-v4
+   function toggleDropdown (e) {
+     const _d = $(e.target).closest('.dropdown'),
+       _m = $('.dropdown-menu', _d);
+     setTimeout(function(){
+       const shouldOpen = e.type !== 'click' && _d.is(':hover');
+       _m.toggleClass('show', shouldOpen);
+       _d.toggleClass('show', shouldOpen);
+       $('[data-toggle="dropdown"]', _d).attr('aria-expanded', shouldOpen);
+     }, e.type === 'mouseleave' ? 50 : 0);
+   }
+
+   $('body')
+     .on('mouseenter mouseleave','.dropdown',toggleDropdown)
+     .on('click', '.dropdown-menu a', toggleDropdown);
