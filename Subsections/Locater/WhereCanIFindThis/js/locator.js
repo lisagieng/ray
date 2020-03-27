b4968619e2efefaa99b2889b8b6cfa5ef538b6d0
hyburge <56616573+hyburge@users.noreply.github.com>
2020-03-27 09:55:18 -0400

    added search box to map

--- Subsections/Locater/WhereCanIFindThis/js/locator.js
+++ Subsections/Locater/WhereCanIFindThis/js/locator.js
@@ -1,5 +1,6 @@
-//js code taken from google maps api tutorial page:
+//js code for search box & geolocation taken from google maps api tutorial page:
 //https://developers.google.com/maps/documentation/javascript/examples/map-geolocation
+//https://developers.google.com/maps/documentation/javascript/examples/places-searchbox
 
 var map;
 function initMap() {
@@ -30,7 +31,7 @@ function initMap() {
 	
 	service = new google.maps.places.PlacesService(map);
 	
-	if(document.getElementById("compost heap").checked){
+	/*if(document.getElementById("compost heap").checked){
 		var request = {
 			query: 'compost heap',
 			fields: ['name', 'geometry'],
@@ -89,8 +90,61 @@ function initMap() {
             map.setCenter(results[0].geometry.location);
           }
         });		
-	}
+	}*/
 	
+	
+	var input = document.getElementById('pac-input');
+        var searchBox = new google.maps.places.SearchBox(input);
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+		
+        map.addListener('bounds_changed', function() {
+          searchBox.setBounds(map.getBounds());
+        });
+
+        var markers = [];
+		
+        searchBox.addListener('places_changed', function() {
+          var places = searchBox.getPlaces();
+
+          if (places.length == 0) {
+            return;
+          }
+
+          markers.forEach(function(marker) {
+            marker.setMap(null);
+          });
+          markers = [];
+
+          var bounds = new google.maps.LatLngBounds();
+          places.forEach(function(place) {
+            if (!place.geometry) {
+              console.log("Returned place contains no geometry");
+              return;
+            }
+            var icon = {
+              url: place.icon,
+              size: new google.maps.Size(71, 71),
+              origin: new google.maps.Point(0, 0),
+              anchor: new google.maps.Point(17, 34),
+              scaledSize: new google.maps.Size(25, 25)
+            };
+
+            markers.push(new google.maps.Marker({
+              map: map,
+              icon: icon,
+              title: place.name,
+              position: place.geometry.location
+            }));
+
+            if (place.geometry.viewport) {
+              bounds.union(place.geometry.viewport);
+            } else {
+              bounds.extend(place.geometry.location);
+            }
+          });
+          map.fitBounds(bounds);
+        });
+      
 }
 
 function createMarker(place) {
