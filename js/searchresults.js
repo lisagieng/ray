b6f71d0189e3da73ba2c2ab7fd16c1d196b75047
lisagieng <lisagieng@gmail.com>
2020-04-14 19:26:57 -0400

    Added change functions for "not sure where to get started"

--- js/searchresults.js
+++ js/searchresults.js
@@ -1,11 +1,10 @@
-///////////////////////////////////////////////////////////////////////
 function sorrysearch() {
   var sorry = "Sorry, the search function for the recycling hasn't been implemented yet.";
   $("#mybutton").prop("disabled", true);
   $("#searchme").append(sorry);
 }
 
-$.validator.addMethod('selectcheck', function (value) {
+/*$.validator.addMethod('selectcheck', function (value) {
     return (value != '0');
 });
 
@@ -18,31 +17,81 @@ $(function () {
         sel2: {selectcheck: true}
       }
     });
-});
+});*/
 
-/*$(function() {
-  $("#sel1").change(function() {
-    if($("#sel2").val() == "blank2" && $("#sel1").val() == "blank1"){
-      $("#testing").hide();
+
+$(function () {
+  $("#sel1").change(function() { // when first box changes
+    if ($("#sel1").val() == "0" && $("#sel2").val() == "0"){ //if no select
+      $("#results").html(" ");
     }
-    if ($("#sel2").val() == "blank2" || $("#sel1").val() == "blank1"){
-      $("#testing").show();
+    if ($("#sel1").val() == "0" || $("#sel2").val() == "0"){ //if one select
+      $("#results").html(" ");
     }
-    if ($("#sel1").val() != "blank1" && $("#sel2").val() != "blank2"){
-      $("#testing").hide();
+
+    ////////// HOW
+    if ($("#sel1").val() == "how" && $("#sel2").val() == "planet"){ // how/planet
+      $("#results").html("<p>how/planet</p>");
+    }
+    if ($("#sel1").val() == "how" && $("#sel2").val() == "sustainable"){ // how/sustainable
+      $("#results").html("<p>how/sustainable</p>");
+    }
+    if ($("#sel1").val() == "how" && $("#sel2").val() == "recycle"){ // how/educate
+      $("#results").html("<p>how/educate</p>");
+    }
+    if ($("#sel1").val() == "how" && $("#sel2").val() == "educate"){ // how/educate
+      $("#results").html("<p>how/educate</p>");
+    }
+
+    ////////// WHERE
+    if ($("#sel1").val() == "where" && $("#sel2").val() == "planet"){ // where/planet
+      $("#results").html("<p>where/planet</p>");
+    }
+    if ($("#sel1").val() == "where" && $("#sel2").val() == "sustainable"){ // where/sustainable
+      $("#results").html("<p>where/sustainable</p>");
+    }
+    if ($("#sel1").val() == "where" && $("#sel2").val() == "recycle"){ // where/educate
+      $("#results").html("<p>where/educate</p>");
+    }
+    if ($("#sel1").val() == "where" && $("#sel2").val() == "educate"){ // where/educate
+      $("#results").html("<p>where/educate</p>");
     }
   });
 
-  $("#sel2").change(function () {
-    if($("#sel2").val() == "blank2" && $("#sel1").val() == "blank1"){
-      $("#testing").hide();
+  $("#sel2").change(function() { // when second box changes
+    if ($("#sel1").val() == "0" && $("#sel2").val() == "0"){ //if no select
+      $("#results").html(" ");
     }
-    if ($("#sel1").val() == "blank1" || $("#sel2").val() == "blank2"){
-      $("#testing").show();
+    if ($("#sel1").val() == "0" || $("#sel2").val() == "0"){ //if one select
+      $("#results").html(" ");
     }
-    if ($("#sel2").val() != "blank2" && $("#sel1").val() != "blank1"){
-{      $("#testing").hide();
-    }}
+
+    ////////// HOW
+    if ($("#sel1").val() == "how" && $("#sel2").val() == "planet"){ // how/planet
+      $("#results").html("<p>how/planet</p>");
+    }
+    if ($("#sel1").val() == "how" && $("#sel2").val() == "sustainable"){ // how/sustainable
+      $("#results").html("<p>how/sustainable</p>");
+    }
+    if ($("#sel1").val() == "how" && $("#sel2").val() == "recycle"){ // how/recycle
+      $("#results").html("<p>how/recycle</p>");
+    }
+    if ($("#sel1").val() == "how" && $("#sel2").val() == "educate"){ // how/educate
+      $("#results").html("<p>how/educate</p>");
+    }
+
+    ////////// WHERE
+    if ($("#sel1").val() == "where" && $("#sel2").val() == "planet"){ // where/planet
+      $("#results").html("<p>where/planet</p>");
+    }
+    if ($("#sel1").val() == "where" && $("#sel2").val() == "sustainable"){ // where/sustainable
+      $("#results").html("<p>where/sustainable</p>");
+    }
+    if ($("#sel1").val() == "where" && $("#sel2").val() == "recycle"){ // where/recycle
+      $("#results").html("<p>where/recycle</p>");
+    }
+    if ($("#sel1").val() == "where" && $("#sel2").val() == "educate"){ // where/educate
+      $("#results").html("<p>where/educate</p>");
+    }
   });
 });
-*/
