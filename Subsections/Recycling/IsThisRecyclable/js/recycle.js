9911075f665dc6630fcb7a81aed1b80ce400c2cc
lisagieng <lisagieng@gmail.com>
2020-04-15 16:57:05 -0400

    Update recycle.js

--- Subsections/Recycling/IsThisRecyclable/js/recycle.js
+++ Subsections/Recycling/IsThisRecyclable/js/recycle.js
@@ -47,14 +47,7 @@ $(function() {
     var more = responseObject.items[i].moreInfo;
 
     myItems += "<tr><td>" + name + "</td><td><a target='_blank' href='" + image + "'><img src='" + image + "' alt='Click to expand image' class='images'></a></td><td>" + material + "</td><td>" + recycle + "</td><td>" + more + "</td></tr>";
-
-  /*  if (i != Object.keys(responseObject.items).length){
-        tags += "'" + name + "',";
-    }
-    else {
-      tags += "'" + name + "']";
-    }
-  }*/
+  }
   $("#resultsHere").html(myItems);
 /*  console.log(tags);
     $( "#searchIcon" ).autocomplete({
