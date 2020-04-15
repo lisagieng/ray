0b91748aba5b6427d18fe5d4b94d056c61a55cd7
lisagieng <lisagieng@gmail.com>
2020-04-14 20:33:30 -0400

    Working on AJAX navbar

--- js/home.js
+++ js/home.js
@@ -1,93 +1,98 @@
-// Implementing dropdown menu with hover
-//https://stackoverflow.com/questions/42183672/how-to-implement-a-navbar-dropdown-hover-in-bootstrap-v4
-function toggleDropdown (e) {
-  const _d = $(e.target).closest('.dropdown'),
-    _m = $('.dropdown-menu', _d);
-  setTimeout(function(){
-    const shouldOpen = e.type !== 'click' && _d.is(':hover');
-    _m.toggleClass('show', shouldOpen);
-    _d.toggleClass('show', shouldOpen);
-    $('[data-toggle="dropdown"]', _d).attr('aria-expanded', shouldOpen);
-  }, e.type === 'mouseleave' ? 50 : 0);
+function sorrysearch() {
+  var sorry = "Sorry, the search function for the recycling hasn't been linked here yet. Please use the link above to get to the search page!";
+  $("#mybutton").prop("disabled", true);
+  $("#searchme").append(sorry);
 }
 
-$('body')
-  .on('mouseenter mouseleave','.dropdown',toggleDropdown)
-  .on('click', '.dropdown-menu a', toggleDropdown);
-///////////////////////////////////////////////////////////////////
-
-///////////////////////////////////////////////////////////////////
-// News Feed loader API
-// https://rss2json.com/google-feed-api-alternative
+// jQuery validation
+$.validator.addMethod('selectcheck', function (value) {
+    return (value != '0');
+});
 
 $(function () {
-google.load("feeds", "1");
-
-   function initialize() {
-     var feed = new google.feeds.Feed("https://www.sciencedaily.com/rss/top/environment.xml");
-     feed.load(function(result) {
-       document.getElementById("feedControl").style.visibility = "hidden";
-       if (!result.error) {
-         var container = document.getElementById("feed");
-         for (var i = 0; i < result.feed.entries.length; i++) {
-           var entry = result.feed.entries[i];
-           var div = document.createElement("div");
-           var readmore = "<a target='_blank' href='" + entry.link + "'><small>  Read more.</small></a>"
-           div.innerHTML = "<a target='_blank' href='" + entry.link + "'>" + entry.title + "</a><small> " + entry.publishedDate + "</small><p>" + entry.content + readmore + "</p><hr>";
-           container.appendChild(div);
-         }
-       }
-     });
-   }
-   google.setOnLoadCallback(initialize);
+    $('#myForm').validate({ // initialize the plugin
+      errorPlacement: function(error, element) {},
+      errorClass: "myselect",
+      rules: {
+        sel1: {selectcheck: true},
+        sel2: {selectcheck: true}
+      }
+    });
 });
-///////////////////////////////////////////////////////////////////////
 
-///////////////////////////////////////////////////////////////////////
-// for contact us submission
-// hosted by an external server https://formspree.io/
-window.addEventListener("DOMContentLoaded", function() {
+// on select box changes
+$(function () {
+  $("#sel1").change(function() { // when first box changes
+    if ($("#sel1").val() == "0" && $("#sel2").val() == "0"){ //if no select
+      $("#results").html(" ");
+    }
+    if ($("#sel1").val() == "0" || $("#sel2").val() == "0"){ //if one select
+      $("#results").html(" ");
+    }
 
-   // get the form elements defined in your form HTML above
+    ////////// HOW
+    if ($("#sel1").val() == "how" && $("#sel2").val() == "planet"){ // how/planet
+      $("#results").html("<p>how/planet</p>");
+    }
+    if ($("#sel1").val() == "how" && $("#sel2").val() == "sustainable"){ // how/sustainable
+      $("#results").html("<p>how/sustainable</p>");
+    }
+    if ($("#sel1").val() == "how" && $("#sel2").val() == "recycle"){ // how/educate
+      $("#results").html("<p>how/educate</p>");
+    }
+    if ($("#sel1").val() == "how" && $("#sel2").val() == "educate"){ // how/educate
+      $("#results").html("<p>how/educate</p>");
+    }
 
-   var form = document.getElementById("my-form");
-   var button = document.getElementById("my-form-button");
-   var status = document.getElementById("my-form-status");
+    ////////// WHERE
+    if ($("#sel1").val() == "where" && $("#sel2").val() == "planet"){ // where/planet
+      $("#results").html("<p>where/planet</p>");
+    }
+    if ($("#sel1").val() == "where" && $("#sel2").val() == "sustainable"){ // where/sustainable
+      $("#results").html("<p>where/sustainable</p>");
+    }
+    if ($("#sel1").val() == "where" && $("#sel2").val() == "recycle"){ // where/educate
+      $("#results").html("<p>where/educate</p>");
+    }
+    if ($("#sel1").val() == "where" && $("#sel2").val() == "educate"){ // where/educate
+      $("#results").html("<p>where/educate</p>");
+    }
+  });
 
-   // Success and Error functions for after the form is submitted
+  $("#sel2").change(function() { // when second box changes
+    if ($("#sel1").val() == "0" && $("#sel2").val() == "0"){ //if no select
+      $("#results").html(" ");
+    }
+    if ($("#sel1").val() == "0" || $("#sel2").val() == "0"){ //if one select
+      $("#results").html(" ");
+    }
 
-   function success() {
-     form.reset();
-     //button.style = "display: none ";
-     status.innerHTML = "Your submission has been received.";
-   }
+    ////////// HOW
+    if ($("#sel1").val() == "how" && $("#sel2").val() == "planet"){ // how/planet
+      $("#results").html("<p>how/planet</p>");
+    }
+    if ($("#sel1").val() == "how" && $("#sel2").val() == "sustainable"){ // how/sustainable
+      $("#results").html("<p>how/sustainable</p>");
+    }
+    if ($("#sel1").val() == "how" && $("#sel2").val() == "recycle"){ // how/recycle
+      $("#results").html("<p>how/recycle</p>");
+    }
+    if ($("#sel1").val() == "how" && $("#sel2").val() == "educate"){ // how/educate
+      $("#results").html("<p>how/educate</p>");
+    }
 
-   function error() {
-     status.innerHTML = "Oops! There was a problem.";
-   }
-
-   // handle the form submission event
-
-   form.addEventListener("submit", function(ev) {
-     ev.preventDefault();
-     var data = new FormData(form);
-     ajax(form.method, form.action, data, success, error);
-   });
- });
-
- // helper function for sending an AJAX request
-
- function ajax(method, url, data, success, error) {
-   var xhr = new XMLHttpRequest();
-   xhr.open(method, url);
-   xhr.setRequestHeader("Accept", "application/json");
-   xhr.onreadystatechange = function() {
-     if (xhr.readyState !== XMLHttpRequest.DONE) return;
-     if (xhr.status === 200) {
-       success(xhr.response, xhr.responseType);
-     } else {
-       error(xhr.status, xhr.response, xhr.responseType);
-     }
-   };
-   xhr.send(data);
- }
+    ////////// WHERE
+    if ($("#sel1").val() == "where" && $("#sel2").val() == "planet"){ // where/planet
+      $("#results").html("<p>where/planet</p>");
+    }
+    if ($("#sel1").val() == "where" && $("#sel2").val() == "sustainable"){ // where/sustainable
+      $("#results").html("<p>where/sustainable</p>");
+    }
+    if ($("#sel1").val() == "where" && $("#sel2").val() == "recycle"){ // where/recycle
+      $("#results").html("<p>where/recycle</p>");
+    }
+    if ($("#sel1").val() == "where" && $("#sel2").val() == "educate"){ // where/educate
+      $("#results").html("<p>where/educate</p>");
+    }
+  });
+});
---
+++ js/index.js
@@ -0,0 +1,100 @@
+// Implementing dropdown menu with hover
+//https://stackoverflow.com/questions/42183672/how-to-implement-a-navbar-dropdown-hover-in-bootstrap-v4
+function toggleDropdown (e) {
+  const _d = $(e.target).closest('.dropdown'),
+    _m = $('.dropdown-menu', _d);
+  setTimeout(function(){
+    const shouldOpen = e.type !== 'click' && _d.is(':hover');
+    _m.toggleClass('show', shouldOpen);
+    _d.toggleClass('show', shouldOpen);
+    $('[data-toggle="dropdown"]', _d).attr('aria-expanded', shouldOpen);
+  }, e.type === 'mouseleave' ? 50 : 0);
+}
+
+$('body')
+  .on('mouseenter mouseleave','.dropdown',toggleDropdown)
+  .on('click', '.dropdown-menu a', toggleDropdown);
+///////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////
+// News Feed loader API
+// https://rss2json.com/google-feed-api-alternative
+
+$(function () {
+google.load("feeds", "1");
+
+   function initialize() {
+     var feed = new google.feeds.Feed("https://www.sciencedaily.com/rss/top/environment.xml");
+     feed.load(function(result) {
+       document.getElementById("feedControl").style.visibility = "hidden";
+       if (!result.error) {
+         var container = document.getElementById("feed");
+         for (var i = 0; i < result.feed.entries.length; i++) {
+           var entry = result.feed.entries[i];
+           var div = document.createElement("div");
+           var readmore = "<a target='_blank' href='" + entry.link + "'><small>  Read more.</small></a>"
+           div.innerHTML = "<a target='_blank' href='" + entry.link + "'>" + entry.title + "</a><small> " + entry.publishedDate + "</small><p>" + entry.content + readmore + "</p><hr>";
+           container.appendChild(div);
+         }
+       }
+     });
+   }
+   google.setOnLoadCallback(initialize);
+});
+///////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////
+// for contact us submission
+// hosted by an external server https://formspree.io/
+window.addEventListener("DOMContentLoaded", function() {
+
+   // get the form elements defined in your form HTML above
+
+   var form = document.getElementById("my-form");
+   var button = document.getElementById("my-form-button");
+   var status = document.getElementById("my-form-status");
+
+   // Success and Error functions for after the form is submitted
+
+   function success() {
+     form.reset();
+     //button.style = "display: none ";
+     status.innerHTML = "Your submission has been received.";
+   }
+
+   function error() {
+     status.innerHTML = "Oops! There was a problem.";
+   }
+
+   // handle the form submission event
+
+   form.addEventListener("submit", function(ev) {
+     ev.preventDefault();
+     var data = new FormData(form);
+     ajax(form.method, form.action, data, success, error);
+   });
+ });
+
+ // helper function for sending an AJAX request
+
+ function ajax(method, url, data, success, error) {
+   var xhr = new XMLHttpRequest();
+   xhr.open(method, url);
+   xhr.setRequestHeader("Accept", "application/json");
+   xhr.onreadystatechange = function() {
+     if (xhr.readyState !== XMLHttpRequest.DONE) return;
+     if (xhr.status === 200) {
+       success(xhr.response, xhr.responseType);
+     } else {
+       error(xhr.status, xhr.response, xhr.responseType);
+     }
+   };
+   xhr.send(data);
+ }
+
+ $(document).ready(function(){
+    $("#div1").load("subsections/home.html");
+
+    $("#additionalResources").click(function() {
+      $("#div1").load("subsections/additional.html");
+});
