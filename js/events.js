ea3e8cdb89cfc984c5b18693ce2ea94c1611e198
lisagieng <lisagieng@gmail.com>
2020-04-15 18:03:32 -0400

    Adding user submission forms

--- js/events.js
+++ js/events.js
@@ -1,20 +1,3 @@
-// Implementing dropdown menu with hover
-//https://stackoverflow.com/questions/42183672/how-to-implement-a-navbar-dropdown-hover-in-bootstrap-v4
-   function toggleDropdown (e) {
-     const _d = $(e.target).closest('.dropdown'),
-       _m = $('.dropdown-menu', _d);
-     setTimeout(function(){
-       const shouldOpen = e.type !== 'click' && _d.is(':hover');
-       _m.toggleClass('show', shouldOpen);
-       _d.toggleClass('show', shouldOpen);
-       $('[data-toggle="dropdown"]', _d).attr('aria-expanded', shouldOpen);
-     }, e.type === 'mouseleave' ? 50 : 0);
-   }
-
-   $('body')
-     .on('mouseenter mouseleave','.dropdown',toggleDropdown)
-     .on('click', '.dropdown-menu a', toggleDropdown);
-
 // parsing json for events
 $(function() {
   var myEvents = "";
@@ -47,3 +30,51 @@ function submission() {
    success = "Event submitted. You will receive a confirmation email regarding your event."
    $("#submitEvent").html(success);
 }
+
+//////////////////
+// parsing form for submission
+window.addEventListener("DOMContentLoaded", function() {
+
+   // get the form elements defined in your form HTML above
+
+   var form = document.getElementById("submitEvent");
+   var button = document.getElementById("my-form-button");
+   var status = document.getElementById("my-form-status");
+
+   // Success and Error functions for after the form is submitted
+
+   function success() {
+     form.reset();
+     button.style = "display: none ";
+     status.innerHTML = "Thanks! Your event has been submitted.";
+   }
+
+   function error() {
+     status.innerHTML = "Oops! There was a problem.";
+   }
+
+   // handle the form submission event
+
+   form.addEventListener("submit", function(ev) {
+     ev.preventDefault();
+     var data = new FormData(form);
+     ajax(form.method, form.action, data, success, error);
+   });
+ });
+
+ // helper function for sending an AJAX request
+
+ function ajax(method, url, data, success, error) {
+   var xhr = new XMLHttpRequest();
+   xhr.open(method, url);
+   xhr.setRequestHeader("Accept", "application/json");
+   xhr.onreadystatechange = function() {
+     if (xhr.readyState !== XMLHttpRequest.DONE) return;
+     if (xhr.status === 200) {
+       success(xhr.response, xhr.responseType);
+     } else {
+       error(xhr.status, xhr.response, xhr.responseType);
+     }
+   };
+   xhr.send(data);
+ }
