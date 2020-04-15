24a6472fe03cc2fb9a2b0389f921be3850fcb9d5
lisagieng <lisagieng@gmail.com>
2020-04-15 17:04:53 -0400

    Update recycle.js

--- Subsections/Recycling/IsThisRecyclable/js/recycle.js
+++ Subsections/Recycling/IsThisRecyclable/js/recycle.js
@@ -25,7 +25,7 @@ function sorrysearch() {
 ///////////////
 $(function() {
   $('#searchIcon').autocomplete({
-    source: "/ray/Subsections/Recycling/IsThisRecyclable/json/database-copy.json"
+    source: "json/database-copy.json"
   });
 });
 
