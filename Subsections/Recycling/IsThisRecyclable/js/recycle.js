1ddb40f3e7b93ec53410cfa15cd793acf8fa0992
lisagieng <lisagieng@gmail.com>
2020-04-15 15:35:56 -0400

    Adding JSON parser to recycling search

--- Subsections/Recycling/IsThisRecyclable/js/recycle.js
+++ Subsections/Recycling/IsThisRecyclable/js/recycle.js
@@ -15,16 +15,37 @@ $('body')
   .on('mouseenter mouseleave','.dropdown',toggleDropdown)
   .on('click', '.dropdown-menu a', toggleDropdown);
 
-
+/////////////
 function sorrysearch() {
 	document.getElementById('hidden0').style.display = 'block';
 	document.getElementById('hidden1').style.display = 'block';
 	document.getElementById('hidden2').style.display = 'block';
 }
 
-function oops() {
-  var sorry = "<h5>Sorry, the recycling search hasn't been implemented yet.<br>Here's a sample result.</h5>";
-  $("#apologies").before(sorry);
-  $(".mysample").css("display", "inline-block");
-  $("#mybutton").prop("disabled", true);
-}
+///////////////
+
+// parsing json for items
+$(function() {
+  var myItems = "";
+
+  var xhttp = new XMLHttpRequest();
+  xhttp.onload = function() {
+    if(xhttp.status === 200) {
+      responseObject = JSON.parse(xhttp.responseText);
+    }
+
+  for(var i = 0; i < Object.keys(responseObject.items).length; i++){
+    var name = responseObject.items[i].name;
+    var image = responseObject.items[i].image;
+    var material = responseObject.items[i].commonlyMadeOf;
+    var recycle = responseObject.items[i].recyclable;
+    var more = responseObject.items[i].moreInfo;
+
+    myItems += "<tr><td>" + name + "</td><td>" + image + "</td><td>" + material + "</td><td>" + recycle + "</td><td>" + more + "</td></tr>";
+    }
+    $("#resultsHere").html(myItems);
+  }
+  xhttp.open("GET", "json/database.json", true);
+  xhttp.send('');
+
+});
