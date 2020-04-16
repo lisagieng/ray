4667202b6b21b355add651b9fdc04bc884932252
lisagieng <lisagieng@gmail.com>
2020-04-16 16:50:38 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -79,8 +79,8 @@ $(function() {
     history.pushState('Recycling & You', 'Recycling & You', '/ray/index.html');
   });
   $("#events").click(function() {
-    $("#div1").load("/ray/Subsections/Locator/Events/Events.html");
-      history.pushState('Events', 'Recycling & You', '/ray/Subsections/Events/Events.html');
+    $("#div1").load("/ray/Subsections/Locater/Events/Events.html");
+      history.pushState('Events', 'Recycling & You', '/ray/Subsections/Locater/Events/Events.html');
   });
   $("#additionalResources").click(function() {
     $("#div1").load("/ray/Subsections/AdditionalResources.html");
