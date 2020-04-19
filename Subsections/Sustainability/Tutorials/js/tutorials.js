e81c74e8d84c8cd48b974d76bdc97d3595be421d
hyburge <56616573+hyburge@users.noreply.github.com>
2020-04-19 03:37:02 -0400

    made it say the channel name too

--- Subsections/Sustainability/Tutorials/js/tutorials.js
+++ Subsections/Sustainability/Tutorials/js/tutorials.js
@@ -18,8 +18,11 @@ function fillDescription(){
 	var youtubeID = url.slice(26, 37);
 	console.log(youtubeID);
 	$.getJSON( "https://www.googleapis.com/youtube/v3/videos?part=id%2Csnippet&id=" + youtubeID + "&key=AIzaSyCbtewZ7C10gOc9eyjccWkBjRFsFmAFxKE", function( data ) {
-    console.log(data.items[0].snippet.localized.title);
-	document.getElementById("videoTitle").innerHTML = data.items[0].snippet.localized.title;
-	document.getElementById("videoDescription").innerHTML = data.items[0].snippet.localized.description;
-});
+		console.log(data.items[0].snippet.channelId);
+		document.getElementById("videoTitle").innerHTML = data.items[0].snippet.title;
+		document.getElementById("videoDescription").innerHTML = data.items[0].snippet.description;
+		$.getJSON("https://www.googleapis.com/youtube/v3/channels?part=id%2Csnippet&id=" + data.items[0].snippet.channelId + "&key=AIzaSyCbtewZ7C10gOc9eyjccWkBjRFsFmAFxKE", function( channel ) {
+			document.getElementById("videoTitle").innerHTML += "</h3><h4> by </h4><h3>" + channel.items[0].snippet.title;
+		});
+	});
 };
